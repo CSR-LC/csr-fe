f1c3e6ae9bdbd151822b2a329a669830d5611e96
Ilya Denezhkin <ilya_denezhkin@epam.com>
2022-11-29 01:34:12 +0400

    feat(add toast): add toast - fix threads

--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -1,15 +1,15 @@
-import {Component, ChangeDetectionStrategy, Inject} from '@angular/core';
-import {MAT_SNACK_BAR_DATA} from "@angular/material/snack-bar";
-import {NotificationTypes} from "@shared/constants/notification.enum";
-import {NotificationData} from "@shared/constants/notification";
+import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { NotificationTypes } from '@shared/constants/notification.enum';
+import { NotificationData } from '@shared/constants/notification';
 
 @Component({
   selector: 'lc-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotificationComponent {
   readonly notificationTypes = NotificationTypes;
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: NotificationData){}
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: NotificationData) {}
 }
--- src/app/shared/services/notifications/notifications.service.ts
+++ src/app/shared/services/notifications/notifications.service.ts
@@ -1,55 +1,51 @@
 import { Injectable } from '@angular/core';
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {MatSnackBarConfig} from "@angular/material/snack-bar/snack-bar-config";
-import {NotificationTypes} from "@shared/constants/notification.enum";
-import {NotificationComponent} from "@shared/components/notification/notification.component";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarConfig } from '@angular/material/snack-bar/snack-bar-config';
+import { NotificationTypes } from '@shared/constants/notification.enum';
+import { NotificationComponent } from '@shared/components/notification/notification.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationsService {
-
   private readonly config: MatSnackBarConfig = {
     verticalPosition: 'top',
     horizontalPosition: 'center',
-    duration: 5000
-  }
+    duration: 5000,
+  };
 
-  constructor(private notification: MatSnackBar) { }
+  constructor(private notification: MatSnackBar) {}
 
   openError(message: string) {
     const config = {
       ...this.config,
-      data:
-        {
-          type:NotificationTypes.Error,
-          message
-        }
-    }
-    this.notification.openFromComponent(NotificationComponent, { ...config })
+      data: {
+        type: NotificationTypes.Error,
+        message,
+      },
+    };
+    this.notification.openFromComponent(NotificationComponent, config);
   }
 
   openWarning(message: string) {
     const config = {
       ...this.config,
-      data:
-        {
-          type:NotificationTypes.Warning,
-          message
-        }
-    }
-    this.notification.openFromComponent(NotificationComponent, { ...config })
+      data: {
+        type: NotificationTypes.Warning,
+        message,
+      },
+    };
+    this.notification.openFromComponent(NotificationComponent, config);
   }
 
   openSuccess(message: string) {
     const config = {
       ...this.config,
-      data:
-        {
-          type:NotificationTypes.Success,
-          message
-        }
-    }
-    this.notification.openFromComponent(NotificationComponent, { ...config })
+      data: {
+        type: NotificationTypes.Success,
+        message,
+      },
+    };
+    this.notification.openFromComponent(NotificationComponent, config);
   }
 }
