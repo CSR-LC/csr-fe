06ba609112f6bf6727f4a5286c9c03adea8baf15
Your Name <you@example.com>
2022-11-28 11:38:20 +0400

    Adjust Equipment item card to a new design

--- src/app/catalog/containers/equipment-item/equipment-item.component.ts
+++ src/app/catalog/containers/equipment-item/equipment-item.component.ts
@@ -1,9 +1,16 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {CatalogController} from "../../services";
-import {ActivatedRoute} from "@angular/router";
-import {first, Observable} from "rxjs";
-import {Equipment} from "../../models/equipment";
-import {MainPageHeaderService} from "@shared/services/main-page-header.service";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { CatalogController } from '../../services';
+import { ActivatedRoute } from '@angular/router';
+import { first, Observable } from 'rxjs';
+import { Equipment } from '../../models/equipment';
+import { MainPageHeaderService } from '@shared/services/main-page-header.service';
 
 @Component({
   selector: 'lc-equipment-item',
@@ -17,8 +24,9 @@ export class EquipmentItemComponent implements OnInit {
 
   catalog$ = this.controller.catalog$;
   equipment?: Equipment;
+  counter: number = 0;
 
-  readonly defaultImage = "./assets/img/no-photo.png";
+  readonly defaultImage = './assets/img/no-photo.png';
 
   constructor(
     private readonly controller: CatalogController,
@@ -30,24 +38,40 @@ export class EquipmentItemComponent implements OnInit {
   ngOnInit(): void {
     this.controller.getCatalog();
 
-    this.controller.getEquipmentItemInfo(
-      this.route.snapshot.params['id']
-    ).subscribe(item => {
-      this.mainPageHeaderService.setPageTitle(item.name);
-      this.equipment = item;
-      this.setPhoto(item);
-      this.cdr.markForCheck();
-    });
+    this.controller
+      .getEquipmentItemInfo(this.route.snapshot.params['id'])
+      .subscribe((item) => {
+        this.mainPageHeaderService.setPageTitle(item.name);
+        this.equipment = item;
+        this.setPhoto(item);
+        this.cdr.markForCheck();
+      });
   }
 
   private setPhoto(equipment: Equipment): void {
-    this.controller.getPhotoById(equipment.photoID).subscribe(res => {
+    this.controller.getPhotoById(equipment.photoID).subscribe((res) => {
       const urlCreator = window.URL || window.webkitURL;
       const url = urlCreator.createObjectURL(res);
 
       if (!this.image) return;
 
       this.image.nativeElement.src = url;
-    })
+    });
+  }
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  get cannotIncrease(): boolean {
+    return this.counter === this.equipment?.maximumDays;
+  }
+
+  public increase(): void {
+    this.counter++;
+  }
+
+  public decrease(): void {
+    this.counter--;
   }
 }
