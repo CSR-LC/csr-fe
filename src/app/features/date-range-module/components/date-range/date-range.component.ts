6ae185f6ad0ba8bff58e849f602e4046a61b1b94
Ala Tkachuk <regicter1810@mail.ru>
2023-03-18 19:47:30 +0300

    feat: create folder structure

---
+++ src/app/features/date-range-module/components/date-range/date-range.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'lc-date-range',
+  templateUrl: './date-range.component.html',
+  styleUrls: ['./date-range.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DateRangeComponent {}
---
+++ src/app/features/date-range-module/date-range-module.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateRangeComponent } from './components/date-range/date-range.component';
+
+@NgModule({
+  declarations: [DateRangeComponent],
+  imports: [CommonModule],
+})
+export class DateRangeModuleModule {}
