a429b8d06a723444dad023b2bcaf56437f41951a
Alexei Popov <aleksei.popov.vl@gmail.com>
2024-03-20 14:53:43 +0100

    feat: test fixed

--- src/app/shared/components/table-filter/table-filter.component.spec.ts
+++ src/app/shared/components/table-filter/table-filter.component.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableFilterComponent } from './table-filter.component';
+import { Entity } from '@app/shared/models';
 
 xdescribe('TableFilterComponent', () => {
-  let component: TableFilterComponent;
-  let fixture: ComponentFixture<TableFilterComponent>;
+  let component: TableFilterComponent<Entity>;
+  let fixture: ComponentFixture<TableFilterComponent<Entity>>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [TableFilterComponent],
     });
-    fixture = TestBed.createComponent(TableFilterComponent);
+    fixture = TestBed.createComponent(TableFilterComponent<Entity>);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
