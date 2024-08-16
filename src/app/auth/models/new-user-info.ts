d50ec7ae9270af946feac92228633cfa4f0c7c40
Vitaliy Pak <vitaliy_pak@epam.com>
2024-08-16 10:04:00 +0500

    feat: user profile - removed name field, added no applications message

--- src/app/auth/models/new-user-info.ts
+++ src/app/auth/models/new-user-info.ts
@@ -1,10 +1,8 @@
 import { UserType } from './user-type';
 
-// TODO: correct the interface when back remove required fields
 export type NewUserInfo = {
   login: string;
   email: string;
   password: string;
   type: UserType;
-  name: string;
 };
