429af603274910876560c610a027ed1a4f42baeb
Vitaliy Pak <vitaliy_pak@epam.com>
2025-12-19 19:16:33 +0500

    refactor: update API service to use Pick for updateOrder payload

--- src/app/my-applications/services/api/api.service.ts
+++ src/app/my-applications/services/api/api.service.ts
@@ -15,4 +15,16 @@ export class ApiService {
       `${ApiService.ORDERS_BASE_URL}?status=${status}&limit=${limit}&offset=${offset}`,
     );
   }
+
+  getOrder(orderId: string): Observable<Application> {
+    return this.http.get<Application>(`${ApiService.ORDERS_BASE_URL}/${orderId}`);
+  }
+
+  updateOrder(orderId: string, payload: Pick<Application, 'description' | 'quantity' | 'rent_end' | 'rent_start'>): Observable<Application> {
+    return this.http.put<Application>(`${ApiService.ORDERS_BASE_URL}/${orderId}`, payload);
+  }
+
+  deleteOrder(orderId: string): Observable<void> {
+    return this.http.delete<void>(`${ApiService.ORDERS_BASE_URL}/${orderId}`);
+  }
 }
